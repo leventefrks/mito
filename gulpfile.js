ad711ffd5e1db32607f5f9f208b9902e3b084771
levente farkas <levente.farkas@gmail.com>
2018-04-18 12:36:39 +0200

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,36 @@ const gulp = require('gulp'),
     prefix = require('gulp-autoprefixer'),
     sass = require('gulp-sass'),
     purify = require('gulp-purifycss'),
+    htmlmin = require('gulp-htmlmin'),
+    plumber = require('gulp-plumber'),
     bs = require('browser-sync').create();
 
 
-gulp.task('styles', ()=>{
+gulp.task('styles', () => {
     return gulp.src('src/scss/base.scss')
-    .pipe(sass())
-    .pipe(prefix('last 2 versions'))
-    .pipe(concat('main.css'))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('dist/css'))
-    .pipe(bs.reload({stream: true}))
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(prefix('last 2 versions'))
+        .pipe(concat('main.css'))
+        .pipe(minifyCSS())
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(bs.reload({ stream: true }))
 });
 
-gulp.task('postcss', function() {
-    return gulp.src('./dist/css/*.css')
-      .pipe(purify('index.html'))
-      .pipe(gulp.dest('./dist/css'));
-  });
+gulp.task('htmlmin', function () {
+    return gulp.src('./*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+});
 
-gulp.task('browser-sync', ()=> {
+gulp.task('postcss', function () {
+    return gulp.src('./dist/css/*.css')
+        .pipe(purify('index.html'))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('browser-sync', () => {
     bs.init({
         server: {
             baseDir: "./"
@@ -31,7 +41,7 @@ gulp.task('browser-sync', ()=> {
     });
 });
 
-gulp.task('watch', ['browser-sync'], ()=> {
-    gulp.watch("src/scss/*.scss", ['styles']);
+gulp.task('watch', ['browser-sync'], () => {
+    gulp.watch("src/scss/*.scss", ['styles', 'htmlmin']);
     gulp.watch("*.html").on('change', bs.reload);
 });
